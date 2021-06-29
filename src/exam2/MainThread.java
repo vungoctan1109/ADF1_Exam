03229deec5076e4138c0df6d67f27329e91ae180
vungoctan1109 <vntan1109@gmail.com>
2021-06-29 20:40:06 +0700

    exam

--- src/exam2/MainThread.java
+++ src/exam2/MainThread.java
@@ -1,18 +1,17 @@
 package exam2;
 
-import exam2.entity.ConversionUtil;
-
-
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class MainThread {
     public static void main(String[] args) {
-        ConversionUtil conversionUtil = new ConversionUtil();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a string:");
         String inputText = scanner.nextLine();
+        StringTokenizer stringTokenizer = new StringTokenizer(inputText);
         System.out.println("Result: ");
-        System.out.println(conversionUtil.converse(inputText));
+        while (stringTokenizer.hasMoreTokens()) {
+            System.out.println(stringTokenizer.nextToken());
+        }
     }
 }
